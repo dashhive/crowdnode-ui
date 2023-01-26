55255de86753a224d181fe2aede7a68249dc5cf0
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-01-25 21:59:41 -0700

    fix: update document types

--- types.js
+++ types.js
@@ -1,6 +1,7 @@
 /**
  * @typedef {import("@plamikcho/pbcrypto").ICrypto} BaseICrypto
- * @typedef {Window & import("@dashincubator/base58check/base58check.js")}
+ * @typedef {Window & import("@dashincubator/base58check/base58check.js") & import("dashsight/dashsight.js").Dashsight & import("dashsight/dashsocket.js").DashSocket & import("dashkeys/dashkeys.js")}
+ * @typedef {Window & import("crowdnode/crowdnode.js")} CrowdNode
  *
  * @typedef {Object} Encrypto
  * @property {BaseICrypto["encrypt"]} encrypt
@@ -13,4 +14,14 @@
  *  foo?: boolean;
  *  bar?: string | null;
  * }} AlternativeDefinitionStyle
+ *
+ * @typedef {{
+ *  encPrivKey?: HTMLElement;
+ *  signupCrowdNodeForm?: HTMLElement;
+ *  acceptCrowdNodeForm?: HTMLElement;
+ *  depositCrowdNodeForm?: HTMLElement;
+ *  privKeyForm?: HTMLElement;
+ *  balanceForm?: HTMLElement;
+ * } & Document} document
+ *
  */
