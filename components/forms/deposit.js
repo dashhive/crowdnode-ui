483f97f4ace48c84f26dbd93302eaa3973b338ab
jojobyte <184880+jojobyte@users.noreply.github.com>
2023-04-26 16:45:28 -0600

    chore: cleanup dead code

--- components/forms/deposit.js
+++ components/forms/deposit.js
@@ -2,8 +2,6 @@ import {
   requestFunds,
 } from '../../lib/ui.js'
 
-// import { isDecryptedPhraseOrWif } from '../../utils.js'
-
 const initialState = {
   id: 'Button',
   name: 'deposit',
@@ -19,23 +17,7 @@ export function setupDepositButton(el, state = {}) {
     ...state,
   }
 
-  // console.log('setupDepositButton state', state)
-
   const form = document.createElement('form')
-
-  // style.textContent = `
-  //   @import url(/index.css);
-  //   fieldset {
-  //     border: 0;
-  //     min-width: 1rem;
-  //   }
-  //   form fieldset {
-  //     min-width: 1rem;
-  //   }
-  //   form fieldset button {
-  //     border: 0 solid transparent;
-  //   }
-  // `
 
   form.classList.add('btn')
 
@@ -47,34 +29,6 @@ export function setupDepositButton(el, state = {}) {
     </fieldset>
   `
 
-  // let handleDepositModal = async event => {
-  //   let returnValue = event?.target?.returnValue
-
-  //   // console.log(
-  //   //   `${state.name} button handleDepositModal`,
-  //   //   returnValue,
-  //   //   state.passphrase?.length
-  //   // )
-
-  //   if (returnValue && returnValue !== 'cancel') {
-  //     state.passphrase = returnValue
-  //   }
-
-  //   if (
-  //     state.passphrase ||
-  //     isDecryptedPhraseOrWif(state.phraseOrWif)
-  //   ) {
-  //     let depositDialog = setupDepositDialog(
-  //       document.querySelector("main"),
-  //       {
-  //         address: state.address,
-  //         passphrase: state.passphrase
-  //       }
-  //     )
-
-  //     depositDialog.showModal()
-  //   }
-  // }
   let handleSubmit = async event => {
     event.preventDefault()
     console.log(`${state.name} button handleSubmit`, event)
@@ -85,19 +39,6 @@ export function setupDepositButton(el, state = {}) {
         ''
       )
     }
-
-    // if(
-    //   !state.passphrase &&
-    //   !isDecryptedPhraseOrWif(state.phraseOrWif)
-    // ) {
-    //   let encryptDialog = await setupEncryptDialog(document.querySelector("main"))
-
-    //   encryptDialog.addEventListener('close', handleDepositModal)
-
-    //   encryptDialog.showModal()
-    // } else {
-    //   await handleDepositModal()
-    // }
   }
 
   form.addEventListener('submit', handleSubmit)
